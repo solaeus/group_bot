56928bc80e9ed9f434c4ebf628a42edcbb763049
Jeff <dev@jeffa.io>
2025-03-09 13:38:31 -0400

    Add events system; Execute commands on message event very slowly

--- src/bot.rs
+++ src/bot.rs
@@ -1,19 +1,34 @@
 /// A bot that buys, sells and trades with players.
 ///
 /// See [main.rs] for an example of how to run this bot.
-use std::{sync::Arc, time::Duration};
+use std::{
+    collections::VecDeque,
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 use log::info;
 use tokio::runtime::Runtime;
 use veloren_client::{addr::ConnectionArgs, Client, ClientType, Event as VelorenEvent};
 use veloren_common::{
     clock::Clock,
-    comp::{invite::InviteKind, ChatType, ControllerInputs},
+    comp::{
+        invite::InviteKind,
+        item::{ItemDesc, ItemI18n},
+        ChatType, ControllerInputs,
+    },
     uid::Uid,
     ViewDistances,
 };
 
 const CLIENT_TPS: Duration = Duration::from_millis(33);
+const BOT_EVENT_INTERVAL: Duration = Duration::from_secs(1);
+
+enum BotEvent {
+    InvitePlayer(Uid),
+    KickPlayer(Uid),
+    SendTell(String, String),
+}
 
 /// An active connection to the Veloren server that will attempt to run every time the `tick`
 /// function is called.
@@ -22,6 +37,11 @@ pub struct Bot {
 
     client: Client,
     clock: Clock,
+
+    events: VecDeque<BotEvent>,
+    last_bot_event: Instant,
+
+    item_i18n: ItemI18n,
 }
 
 impl Bot {
@@ -83,6 +103,9 @@ impl Bot {
             admins,
             client,
             clock,
+            events: VecDeque::new(),
+            last_bot_event: Instant::now(),
+            item_i18n: ItemI18n::new_expect(),
         })
     }
 
@@ -96,6 +119,14 @@ impl Bot {
 
         for event in veloren_events {
             self.handle_veloren_event(event)?;
+        }
+
+        if self.last_bot_event.elapsed() >= BOT_EVENT_INTERVAL {
+            if let Some(next_bot_event) = self.events.pop_front() {
+                self.handle_bot_event(next_bot_event)?;
+            }
+
+            self.last_bot_event = Instant::now();
         }
 
         if !self.client.is_dead() {
@@ -122,109 +153,146 @@ impl Bot {
                 let sender_uid = if let Some(uid) = message.uid() {
                     uid
                 } else {
-                    return Ok(());
+                    return Err("Failed to get sender UID".to_string());
                 };
                 let sender_name = self.find_player_alias(&sender_uid)?.clone();
-
                 let message_parts: Vec<&str> = message
                     .content()
                     .as_plain()
                     .unwrap_or("")
                     .split_whitespace()
                     .collect();
-                let command = message_parts.get(0).ok_or("Failed to get command")?;
+                let command = message_parts.first().ok_or("Failed to get command")?;
                 let args = &message_parts[1..];
 
                 match *command {
                     "inv" => {
                         if !self.admins.contains(&sender_name) {
-                            self.client.send_command(
-                                "tell".to_string(),
-                                vec![sender_name, "You are not an admin".to_string()],
-                            );
+                            self.events.push_back(BotEvent::SendTell(
+                                sender_name.clone(),
+                                "You are not an admin".to_string(),
+                            ));
 
                             return Ok(());
                         }
 
                         if args.is_empty() {
-                            self.client.send_invite(sender_uid, InviteKind::Group);
+                            self.events.push_back(BotEvent::InvitePlayer(sender_uid));
                         } else {
                             for arg in args {
-                                let target_uid =
-                                    self.find_uid(arg).ok_or("Failed to find target uid")?;
-
-                                self.client.send_invite(*target_uid, InviteKind::Group);
-                                self.client.send_command(
-                                    "tell".to_string(),
-                                    vec![sender_name.clone(), format!("Invited {}", arg)],
-                                );
+                                if let Some(uid) = self.find_uid(arg) {
+                                    self.events.push_back(BotEvent::InvitePlayer(*uid));
+                                    self.events.push_back(BotEvent::SendTell(
+                                        sender_name.clone(),
+                                        format!("Invited {}", arg),
+                                    ));
+                                } else {
+                                    self.events.push_back(BotEvent::SendTell(
+                                        sender_name.clone(),
+                                        format!("Failed to find player {}", arg),
+                                    ));
+                                }
                             }
                         }
                     }
                     "kick" => {
                         if !self.admins.contains(&sender_name) {
-                            self.client.send_command(
-                                "tell".to_string(),
-                                vec![sender_name, "You are not an admin".to_string()],
-                            );
+                            self.events.push_back(BotEvent::SendTell(
+                                sender_name,
+                                "You are not an admin".to_string(),
+                            ));
 
                             return Ok(());
                         }
 
                         if args.is_empty() {
-                            self.client.send_command(
-                                "tell".to_string(),
-                                vec![sender_name, "You must specify a player to kick".to_string()],
-                            );
+                            self.events.push_back(BotEvent::SendTell(
+                                sender_name,
+                                "You must specify a player to kick".to_string(),
+                            ));
 
                             return Ok(());
                         }
 
                         for arg in args {
-                            let target_uid =
-                                self.find_uid(arg).ok_or("Failed to find target uid")?;
-
-                            self.client.kick_from_group(*target_uid);
+                            if let Some(uid) = self.find_uid(arg) {
+                                self.events.push_back(BotEvent::KickPlayer(*uid));
+                            } else {
+                                self.events.push_back(BotEvent::SendTell(
+                                    sender_name.clone(),
+                                    format!("Failed to find player {}", arg),
+                                ));
+                            }
                         }
                     }
                     "admin" => {
                         if !self.admins.contains(&sender_name) {
-                            self.client.send_command(
-                                "tell".to_string(),
-                                vec![sender_name, "You are not an admin".to_string()],
-                            );
+                            self.events.push_back(BotEvent::SendTell(
+                                sender_name,
+                                "You are not an admin".to_string(),
+                            ));
 
                             return Ok(());
                         }
 
                         if args.is_empty() {
-                            self.client.send_command(
-                                "tell".to_string(),
-                                vec![
-                                    sender_name,
-                                    "You must specify a player to promote".to_string(),
-                                ],
-                            );
+                            self.events.push_back(BotEvent::SendTell(
+                                sender_name,
+                                "You must specify a player to promote".to_string(),
+                            ));
 
                             return Ok(());
                         }
 
                         for arg in args {
-                            let target_uid =
-                                self.find_uid(arg).ok_or("Failed to find target uid")?;
-                            let target_name = self.find_player_alias(target_uid)?.clone();
+                            if !self.client.players().any(|player| player == *arg) {
+                                self.events.push_back(BotEvent::SendTell(
+                                    sender_name.clone(),
+                                    format!("Failed to find player {}", arg),
+                                ));
 
-                            self.admins.push(target_name.clone());
-                            self.client.send_command(
-                                "tell".to_string(),
-                                vec![sender_name.clone(), format!("Promoted {}", target_name)],
-                            );
+                                continue;
+                            }
+
+                            self.admins.push(arg.to_string());
+                            self.events.push_back(BotEvent::SendTell(
+                                sender_name.clone(),
+                                format!("Promoted {}", arg),
+                            ));
                         }
                     }
                     _ => {}
                 }
             }
+            VelorenEvent::GroupInventoryUpdate(item, uid) => {
+                let (item_name, _) = item.i18n(&self.item_i18n);
+                let recipient_name = self.find_player_alias(&uid)?;
+
+                if item_name.as_plain().unwrap_or("") == "Dwarven Cheese" {
+                    self.events.push_back(BotEvent::SendTell(
+                        recipient_name.clone(),
+                        format!("Congratulations on the cheese, {recipient_name}!"),
+                    ));
+                }
+            }
             _ => (),
+        }
+
+        Ok(())
+    }
+
+    fn handle_bot_event(&mut self, event: BotEvent) -> Result<(), String> {
+        match event {
+            BotEvent::InvitePlayer(uid) => {
+                self.client.send_invite(uid, InviteKind::Group);
+            }
+            BotEvent::KickPlayer(uid) => {
+                self.client.kick_from_group(uid);
+            }
+            BotEvent::SendTell(name, message) => {
+                self.client
+                    .send_command("tell".to_string(), vec![name, message]);
+            }
         }
 
         Ok(())
